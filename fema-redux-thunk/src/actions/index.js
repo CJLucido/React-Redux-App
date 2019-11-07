fbd9606ad5b4ef5d171cb135722dbd370b82100f
CJ Lucido <c.jose.lucido@gmail.com>
2019-11-07 12:52:17 -0800

    added async/currying function to actions that requests data from the FEMA api ordered by state, in the action file

--- fema-redux-thunk/src/actions/index.js
+++ fema-redux-thunk/src/actions/index.js
@@ -1,3 +1,6 @@
+import axios from "axios"
+
+
 export const FEMA_LOAD_SUCCESS = "FEMA_LOAD_SUCCESS"
         
 export const FEMA_LOAD_FAILURE =  "FEMA_LOAD_FAILURE"
@@ -9,3 +12,20 @@ export const femaLoadSuccess = data => ({
     type: FEMA_LOAD_SUCCESS,
     payload: data
 })
+
+export const femaLoadFailure = data => ({
+    type: FEMA_LOAD_FAILURE,
+    payload: data
+})
+
+export const femaLoading = () => ({
+    type: FEMA_LOADING
+})
+
+export const fetchStatesUSA= () => dispatch =>{
+    axios
+    .get('https://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries$orderby=state')
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
+
